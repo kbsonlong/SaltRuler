4914bd8ab40c818cb4779fe382750ef1cd8e88ea
kbsonlong <13610018065@139.com>
2017-03-09 10:43:33 +0800

    添加Zabbix

---
+++ ZABBIX/ZabbixAPI.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+import ConfigParser
+import json
+import urllib2
+
+class ZabbixAPI:
+    def __init__(self):
+        # cf = ConfigParser.ConfigParser()
+        # cf.read("OMS/config.ini")
+        # self.__url = cf.get("zabbix_server","url")
+        # self.__user= cf.get("zabbix_server","user")
+        # self.__password = cf.get("zabbix_server","password")
+        # self.__header = {"Content-Type": "application/json-rpc"}
+        # self.__token_id = self.UserLogin()
+        self.__url = "http://192.168.234.167/api_jsonrpc.php"
+        self.__user = "Admin"
+        self.__password = "pcgames2016"
+        self.__header = {"Content-Type": "application/json-rpc"}
+        self.__token_id = self.UserLogin()
+    #登陆获取token
+    def UserLogin(self):
+        data = {
+            "jsonrpc": "2.0",
+            "method": "user.login",
+            "params": {
+                "user": self.__user,
+                "password": self.__password
+            },
+            "id": 0,
+        }
+        return self.PostRequest(data)
+    #推送请求
+    def PostRequest(self, data):
+        request = urllib2.Request(self.__url,json.dumps(data),self.__header)
+        result = urllib2.urlopen(request)
+        response = json.loads(result.read())
+        try:
+            # print response['result']
+            return response['result']
+        except KeyError:
+            raise KeyError
+    #主机列表
+    def HostGet(self,hostid=None,hostip=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method":"host.get",
+            "params":{
+                "output":"extend",
+                "selectGroups": "extend",
+                "selectParentTemplates": ["templateid","name"],
+                "selectInterfaces": ["interfaceid","ip"],
+                "selectInventory": ["os"],
+                "selectItems":["itemid","name"],
+                "selectGraphs":["graphid","name"],
+                "selectApplications":["applicationid","name"],
+                "selectTriggers":["triggerid","name"],
+                "selectScreens":["screenid","name"]
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        if hostid:
+            data["params"]={
+                "output": "extend",
+                "hostids": hostid,
+                "sortfield": "name"
+            }
+        return  self.PostRequest(data)
+    #主机列表
+    def HostCreate(self,hostname,hostip,groupid=None,templateid=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method":"host.create",
+            "params": {
+                "host": hostname,
+                "interfaces": [
+                    {
+                        "type": 1,
+                        "main": 1,
+                        "useip": 1,
+                        "ip": hostip,
+                        "dns": "",
+                        "port": "10050"
+                    }
+                ],
+                "groups": [
+                    {
+                        "groupid": groupid
+                    }
+                ],
+                "templates": [
+                    {
+                        "templateid": templateid
+                    }
+                ]
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        return  self.PostRequest(data)
+
+    #主机组列表
+    def HostGroupGet(self,hostid=None,itemid=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method":"hostgroup.get",
+            "params":{
+                "output": "extend",
+                "hostids": hostid,
+                "itemids": itemid,
+                "sortfield": "name"
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        return  self.PostRequest(data)
+    #监控项列表
+    def ItemGet(self,hostid=None,itemid=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                "hostids": hostid,
+                "itemids": itemid,
+                "sortfield": "name"
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        return  self.PostRequest(data)
+    #模板列表
+    def TemplateGet(self, hostid=None,templateid=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method": "template.get",
+            "params": {
+                "output": "extend",
+                "hostids": hostid,
+                "templateids": templateid,
+                "sortfield": "name"
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        return  self.PostRequest(data)
+    #图像列表
+    def GraphGet(self,hostid=None,graphid=None):
+        data = {
+            "jsonrpc":"2.0",
+            "method": "graph.get",
+            "params": {
+                "output": "extend",
+                "hostids": hostid,
+                "graphids": graphid,
+                "sortfield": "name"
+            },
+            "auth": self.__token_id,
+            "id":1,
+        }
+        return  self.PostRequest(data)
+    #历史数据
+    def History(self,itemid,data_type):
+        data = {
+            "jsonrpc": "2.0",
+            "method": "history.get",
+            "params": {
+                "output": "extend",
+                "history": data_type,
+                "itemids": itemid,
+                "sortfield": "clock",
+                "sortorder": "DESC",
+                "limit": 30
+            },
+            "auth": self.__token_id,
+            "id": 2
+        }
+        return self.PostRequest(data)
+#测试：python manager.py shell ; from ZABBIX.ZabbixAPI import * ; main()，代码修改了要ctrl+Z退出重进
+def main():
+    zapi=ZabbixAPI()
+    token=zapi.UserLogin()
+    print token
+    #39378ec03aa101c2b17d1d2bd6f4ef16
+    hosts=zapi.HostGet()
+    for host in hosts:
+        print host["host"]
+    groups= zapi.HostGroupGet()
+    for group in groups:
+        print group['groupid']
+
+    for temp in zapi.TemplateGet():
+        print temp["templateid"]
+    #[{u'host': u'Zabbix server', u'hostid': u'10084', u'interfaces': [{u'interfaceid': u'1', u'ip': u'127.0.0.1'}]}]
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ ZABBIX/views.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import  render,render_to_response,get_object_or_404
+from django.http import HttpResponseRedirect,HttpResponse,JsonResponse
+from django.core.urlresolvers import reverse
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required  #setting: LOGIN_URL = '/auth/login/'
+from django.shortcuts import redirect
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import  PasswordChangeForm
+from models import *
+from CMDB.models import *
+from ZabbixAPI import ZabbixAPI
+import json
+from django.forms.models import model_to_dict
+import re
+# from pyzabbix import ZabbixAPI
+import ConfigParser
+import datetime
+import time
+
+@login_required
+def host(request):
+    try:
+        zapi=ZabbixAPI()
+        host_list=zapi.HostGet()
+        group_list=zapi.HostGroupGet()
+        template_list=zapi.TemplateGet()
+    except Exception as e:
+        error=str(e)
+    return render(request, 'ZABBIX/host.html', locals())
+
+
+@login_required
+def host_create(request):
+    name=request.GET.get('name','')
+    ip=request.GET.get('ip','')
+    groupid=request.GET.get('groupid','')
+    templateid=request.GET.get('templateid','')
+
+    try:
+        zapi=ZabbixAPI()
+        hostcreate=zapi.HostCreate(name,ip,groupid,templateid)
+        if hostcreate['hostids']:
+            result=u"主机%s创建成功，ID为%s."%(name,hostcreate['hostids'])
+        else:
+            result=u"主机%s创建失败."%(name)
+    except Exception as e:
+        result=str(e)
+    return JsonResponse(result,safe=False)
+
+
+@login_required
+def template(request):
+    return render(request, 'ZABBIX/template.html', locals())
+@login_required
+def item(request):
+    cf = ConfigParser.ConfigParser()
+    cf.read("OMS/config.ini")
+    itemurl = cf.get("zabbix_server","itemurl")
+
+    hostid=request.GET.get('hostid','')
+    try:
+        zapi=ZabbixAPI()
+        host_list=zapi.HostGet()
+        if hostid:
+            item_list=zapi.ItemGet(hostid=hostid)
+        else:
+            item_list=zapi.ItemGet()
+        #时间戳转化成日期格式
+        for item in item_list:
+            item['lastclock']= time.strftime('%Y/%m/%d %H:%M', time.localtime(float(item['lastclock'])))
+    except Exception as e:
+        error=str(e)
+    return render(request, 'ZABBIX/item.html', locals())
+@login_required
+def graph(request):
+    cf = ConfigParser.ConfigParser()
+    cf.read("OMS/config.ini")
+    graphurl = cf.get("zabbix_server","graphurl")
+
+    hostid=request.GET.get('hostid','')
+    try:
+        zapi=ZabbixAPI()
+        host_list=zapi.HostGet()
+        if hostid:
+            graph_list=zapi.GraphGet(hostid=hostid)
+        else:
+            graph_list=zapi.GraphGet()
+    except Exception as e:
+        error=str(e)
+    return render(request, 'ZABBIX/graph.html', locals())
+@login_required
+def screen(request):
+    return render(request, 'ZABBIX/screen.html', locals())
+@login_required
+def history(request):
+    itemid=request.GET.get('itemid','')
+    data_type=request.GET.get('data_type','0')
+    print data_type
+    try:
+        zapi=ZabbixAPI()
+        if itemid:
+            value=[]
+            clock=[]
+            item=zapi.ItemGet(itemid=itemid)[0]
+            host=zapi.HostGet(hostid=item['hostid'])[0]
+            history_list=zapi.History(itemid,int(data_type))
+            for history in history_list:
+                value.append(float(history['value']))
+                clock.append(time.strftime('%Y/%m/%d %H:%M', time.localtime(float(history['clock']))))
+            print history_list
+    except Exception as e:
+        error=str(e)
+    return render(request, 'ZABBIX/history.html', locals())
