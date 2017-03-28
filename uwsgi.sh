a93764e2f6825b10a334616352a9abb4d78d6b33
kbsonlong <13610018065@139.com>
2017-03-28 16:13:36 +0800

    修改启动脚本

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+BASE_DIR="/data/PRG"
 NAME="SaltRuler"
 if [ ! -n "$NAME" ];then
     echo "no arguments"
     exit;
 fi
-
-basedir=`pwd`
 
 echo $NAME
 ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
@@ -16,9 +15,12 @@ do
     kill -9 $id
     echo "kill $id"
 done
+ps -ef | grep ftp.py | grep -v grep | awk '{print $2}' | xargs kill -9
 echo  "################################################"
-echo >/data/PRG/SaltRuler/uwsgi.log
+echo >$BASE_DIR/$NAME/uwsgi.log
 #/usr/local/python27/bin/uwsgi --ini /data/PRG/SaltRuler/uwsgi.ini
 
-su - mysql -c "/usr/local/python27/bin/uwsgi --ini /data/PRG/SaltRuler/uwsgi.ini"
-su - mysql -c "nohup python $basedir/deploy/ftp.py >>$basedir/uwsgi.log &"
+su - mysql -c "/usr/local/python27/bin/uwsgi --ini $BASE_DIR/$NAME/uwsgi.ini"
+cd $BASE_DIR/$NAME/deploy/
+nohup /usr/local/python27/bin/python ftp.py  &
+ps -ef | grep ftp.py | grep -v grep >>$BASE_DIR/$NAME/uwsgi.log
