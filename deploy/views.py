a587e41f6a7839afbef77ca14e33ea5b7977c5ca
kbsonlong <13610018065@139.com>
2017-03-28 15:29:30 +0800

    文件下载操作审计

--- deploy/views.py
+++ deploy/views.py
@@ -1,9 +1,7 @@
 #coding:utf-8
 import os
 import time
-
 from django.shortcuts import render
-
 from EmpAuth.decorators import login_required
 from SaltRuler.glob_config import glob_config
 from deploy.models import files_history
@@ -33,47 +31,50 @@ def upload_file(request,server_id):
             myFile =request.FILES.get("myfile", None)    # 获取上传的文件，如果没有文件，则默认为None
             server = request.POST.get("server",None)
             dest = request.POST.get('dest','/tmp/')
-            print dest
             mdir = request.POST.get('mdir',None)
             mtime = request.POST.get('mtime',None)
+            dest_path = 'dest='
+            nginx_path = ''
+            sapi = SaltAPI(url=salt_server.url, username=salt_server.username, password=salt_server.password)
+            dir_result = sapi.SaltCmd(client='local', tgt=server, fun='file.directory_exists', arg=dest)['return'][0]
+
             if not myFile:
                 contexts.update({'error':u'请选择上传文件!'})
-                return render(request, 'deploy/uploadfile.html', contexts)
-            if not server:
+                dest_path = u'dest=请选择上传文件!'
+                # return render(request, 'deploy/uploadfile.html', contexts)
+            elif not server:
                 contexts.update({'error': u'目标主机不能为空！！'})
-                return render(request, 'deploy/uploadfile.html', contexts)
-
-
-            ##将文件上传到平台所在服务器
-            destination = open(os.path.join(upload_dir,myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作
-            for chunk in myFile.chunks():      # 分块写入文件
-                destination.write(chunk)
-            destination.close()
-
-            ##调用saltstack cp.get_url模块进行文件分发
-            sapi = SaltAPI(url=salt_server.url, username=salt_server.username, password=salt_server.password)
-            nginx_url = 'http://'+ glob_config('nginx','host') +':' + glob_config('nginx','port') + '/' + upload_dir + '/'
-            ##文件上传后静态服务地址
-            nginx_path = u'%s%s'% (nginx_url ,myFile.name)
-            print nginx_path
-            ##目标存放绝对路径
-            if mtime:
-                dest_path=u'dest=/%s/%s%s'% (dest.strip('/'),myFile.name,time.strftime("%Y%m%d%H%M%S", time.localtime()))
-            else:
-                dest_path = u'dest=/%s/%s' % (dest.strip('/'), myFile.name)
-
-            dir_result = sapi.SaltCmd(client='local', tgt=server, fun='file.directory_exists', arg=dest)['return'][0][server]
-            if not dir_result:
+                dest_path = u'dest=目标主机不能为空！!'
+                # return render(request, 'deploy/uploadfile.html', contexts)
+            elif not dir_result[server]:
                 contexts.update({'error': u'目标主机目录不存在，请选择创建目录！！'})
-                return render(request, 'deploy/uploadfile.html', contexts)
-            if mdir:
-                command = 'mkdir -p %s' % dest
-                sapi.SaltCmd(tgt=server, fun="cmd.run", expr_form='list', arg=command)
-            upload_results = sapi.SaltCmd(tgt=server, fun="cp.get_url",expr_form='list', arg=nginx_path, arg1=dest_path)['return'][0]
-            #curl - k https: // 192.168.62.200:8000 - H "Accept: application/x-yaml" - H "X-Auth-Token: 69ce7566d2f6680f420cf673ab0d3dc8639ce7aa" - d client = 'local' - d tgt = '192.168.62.200,192.168.62.201' - d fun = 'cp.get_url' - d arg = 'http://192.168.62.1/upload/along_logo.png' - d arg = '/tmp/along_logo.png'
-            # upload_results = {'return': [{'192.168.62.200': '/tmp/along_logo.png', '192.168.62.201': '/tmp/along_logo.png'}]}['return'][0]
-            # print server.split(',')
-            contexts.update({'success': u'%s 上传成功!' % upload_results})
+                dest_path = u'dest=目标主机目录不存在，请选择创建目录!'
+                # return render(request, 'deploy/uploadfile.html', contexts)
+            else:
+                ##将文件上传到平台所在服务器
+                destination = open(os.path.join(upload_dir,myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作
+                for chunk in myFile.chunks():      # 分块写入文件
+                    destination.write(chunk)
+                destination.close()
+                ##调用saltstack cp.get_url模块进行文件分发
+
+                nginx_url = 'http://'+ glob_config('nginx','host') +':' + glob_config('nginx','port') + '/' + upload_dir + '/'
+                ##文件上传后静态服务地址
+                nginx_path = u'%s%s'% (nginx_url ,myFile.name)
+
+                ##目标存放绝对路径
+                if mtime:
+                    dest_path=u'dest=/%s/%s%s'% (dest.strip('/'),myFile.name,time.strftime("%Y%m%d%H%M%S", time.localtime()))
+                else:
+                    dest_path = u'dest=/%s/%s' % (dest.strip('/'), myFile.name)
+                if mdir:
+                    command = 'mkdir -p %s' % dest
+                    sapi.SaltCmd(tgt=server, fun="cmd.run", expr_form='list', arg=command)
+                upload_results = sapi.SaltCmd(tgt=server, fun="cp.get_url",expr_form='list', arg=nginx_path, arg1=dest_path)['return'][0]
+                #curl - k https: // 192.168.62.200:8000 - H "Accept: application/x-yaml" - H "X-Auth-Token: 69ce7566d2f6680f420cf673ab0d3dc8639ce7aa" - d client = 'local' - d tgt = '192.168.62.200,192.168.62.201' - d fun = 'cp.get_url' - d arg = 'http://192.168.62.1/upload/along_logo.png' - d arg = '/tmp/along_logo.png'
+                # upload_results = {'return': [{'192.168.62.200': '/tmp/along_logo.png', '192.168.62.201': '/tmp/along_logo.png'}]}['return'][0]
+                # print server.split(',')
+                contexts.update({'success': u'%s 上传成功!' % upload_results})
 
             fh=files_history()
             fh.username=username
@@ -83,8 +84,10 @@ def upload_file(request,server_id):
             fh.active_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             fh.url = nginx_path
             fh.save()
+
     except Exception as e:
         contexts.update({'error':e})
+    print contexts
     return render(request, 'deploy/uploadfile.html', contexts)
 
 @login_required
@@ -104,24 +107,33 @@ def download_file(request,server_id):
         if request.method == 'POST':
             server = request.POST.get("server", None)
             dest = request.POST.get('dest')
+            dir_result = sapi.SaltCmd(client='local', tgt=server, fun='file.directory_exists', arg=dest)['return'][0][server]
             if not server:
                 contexts.update({'error': u'目标主机不能为空！！'})
+                action_result = u'目标主机不能为空！！'
             elif not dest:
                 contexts.update({'error': u'下载文件不能为空！！'})
+                action_result = u'下载文件不能为空！！'
+            elif not dir_result :
+                contexts.update({'error': u'文件或目录不存在'})
+                action_result = u'文件或目录不存在！！'
             else:
                 ##检索目录或者文件
-                dir_result = sapi.SaltCmd(client='local', tgt=server, fun='file.directory_exists', arg=dest)['return'][0][ server]
-                if dir_result:
-                    arg = 'ls %s' % dest
-                    files_list = sapi.SaltCmd(tgt=server, fun="cmd.run", expr_form='list', arg=arg)['return'][0][server]
-                    nginx_url = 'http://' + glob_config('nginx', 'host') + ':' + glob_config('nginx','port') + '/' + upload_dir + '/'
-                    contexts.update({'files_list': files_list.split(), 'nginx_url': nginx_url, 'server': server, 'dest': dest})
-                else:
-                    contexts.update({'error': u'文件或目录不存在'})
-                return render(request,'deploy/download_file.html',contexts)
-
+                arg = 'ls %s' % dest
+                files_list = sapi.SaltCmd(tgt=server, fun="cmd.run", expr_form='list', arg=arg)['return'][0][server]
+                nginx_url = 'http://' + glob_config('nginx', 'host') + ':' + glob_config('nginx','port') + '/' + upload_dir + '/'
+                contexts.update({'files_list': files_list.split(), 'nginx_url': nginx_url, 'server': server, 'dest': dest})
+                action_result = arg
+            fh = files_history()
+            fh.username = username
+            fh.active = 'download'
+            fh.path = action_result
+            fh.remote_server = server
+            fh.active_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            fh.save()
     except Exception as e:
-        pass
+        contexts.update({'error': e})
+
     return render(request,'deploy/download_file.html',contexts)
 
 def download_fun(request,server_id):
@@ -140,14 +152,13 @@ def download_fun(request,server_id):
         files = request.POST.get('myfile')
         server = request.POST.get("server", None)
         dest = request.POST.get('dest')
-        print server ,files, dest
         file_path=dest + '/' + files
-        print file_path
         dir_result = sapi.SaltCmd(client='local', tgt=server, fun='file.directory_exists', arg=file_path)['return'][0][server]
         if dir_result:
             arg = 'ls %s' % file_path
             files_list = sapi.SaltCmd(tgt=server, fun="cmd.run", expr_form='list', arg=arg)['return'][0][server]
             contexts.update({'files_list': files_list.split(),  'server': server, 'dest': file_path})
+            action_result = arg
         else:
             ftp_url = 'http://' + glob_config('ftp', 'host') + ':' + glob_config('ftp','port')
             command = 'python /tmp/ftp_client.py %s %s' % (ftp_url,file_path)
@@ -156,11 +167,17 @@ def download_fun(request,server_id):
                 contexts.update({'code':code})
             nginx_url = 'http://' + glob_config('nginx', 'host') + ':' + glob_config('nginx','port') + '/' + upload_dir + '/'
             contexts.update({'files':files,'nginx_url': nginx_url, 'server': server, 'dest': dest})
-        return render(request, 'deploy/download_file.html', contexts)
+            action_result = file_path
+        fh = files_history()
+        fh.username = username
+        fh.active = 'download'
+        fh.path = action_result
+        fh.remote_server = server
+        fh.active_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        fh.save()
     except Exception as e:
-        print e
+        contexts.update({'error': e})
 
-    print contexts
     return render(request, 'deploy/download_file.html', contexts)
 
 @login_required
