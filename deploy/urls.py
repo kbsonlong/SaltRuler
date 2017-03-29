a8df40340b2e7a5a3f47e0c25102a64ed6a8e81c
kbsonlong <13610018065@139.com>
2017-03-29 11:13:38 +0800

    根据码云代码分析工具修改

--- deploy/urls.py
+++ deploy/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     url(r'^uploadFile/(?P<server_id>[0-9]+)/$', views.upload_file, name='upload_file'),
     url(r'^downloadFile/(?P<server_id>[0-9]+)/$', views.download_file, name='download_file'),
     url(r'^download/(?P<server_id>[0-9]+)/$', views.download_fun, name='download'),
-    url(r'^file_remote/(?P<server_id>[0-9]+)/$', views.file_remote, name='file_remote'),
     url(r'^history/$', views.files_his, name='history'),
 ]
