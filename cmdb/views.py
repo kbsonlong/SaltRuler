2c1a4b86e33de1725cbdc97f4a9ac469e6c5a114
kbsonlong <13610018065@139.com>
2017-03-31 17:17:36 +0800

    加入celery、redis任务队列；celery后台调用saltstack将虚拟主机信息写入数据库； 修改虚拟主机页面，后端响应读取数据库

--- cmdb/views.py
+++ cmdb/views.py
@@ -375,52 +375,11 @@ def server_collect(request,server_id):
                      'mem_size': '%s' % (servers.Mem), 'host_note': '%s' % (servers.Cpu_type)}
         b.append(host_dict)
 
-    print b
     contexts.update({'b':b})
-    # try:
-    #     try:
-    #         salt_server = SaltServer.objects.get(id=server_id)
-    #     except Exception as e:  # id不存在时返回第一个
-    #         salt_server = SaltServer.objects.all()[0]
-    # except Exception as e:
-    #     contexts.update({'error':e})
-    #     return render(request, 'cmdb/host_table.html', contexts)
+    result = server_collects.delay(tgt=tgt,server_id=server_id)
+    print result.ready()
 
-    # server_list = SaltServer.objects.all()
-    # contexts = {'server_list': server_list, 'server_id': server_id}
-    # try:
-    #     try:
-    #         salt_server = SaltServer.objects.get(id=server_id)
-    #     except Exception as e:  # id不存在时返回第一个
-    #         salt_server = SaltServer.objects.all()[0]
-    # except Exception as e:
-    #     contexts.update({'error': e})
-    #
-    # sapi = SaltAPI(url=salt_server.url, username=salt_server.username, password=salt_server.password)
-    # grains = sapi.SaltCmd(tgt=tgt, fun='grains.items', client='local')['return'][0]
-    # minions = sapi.key_list('manage.status', client='runner')['return'][0]
-    # if salt_server and grains:
-    #     for i in grains.keys():
-    #         try:
-    #             server = Servers.objects.get(local_ip=i)
-    #         except:
-    #             server = Servers()
-    #         if i in minions['up']:
-    #             minions_status = '0'
-    #         else:
-    #             minions_status = '1'
-    #         server.hostname = grains[i]['host']
-    #         server.local_ip = grains[i]['ip4_interfaces'][device0][0]
-    #         server.OS = grains[i]['os'] + ' ' + grains[i]['osrelease'] + '-' + grains[i]['osarch']
-    #         server.Mem = grains[i]['mem_total']
-    #         server.Cpus = grains[i]['num_cpus']
-    #         server.Cpu_type = grains[i]['cpu_model']
-    #         server.minion_id = grains[i]['id']
-    #         server.server_status = minions_status
-    #         server.save()
-    #         contexts.update({'success': u'%s 收集成功' % tgt, 'server_id': salt_server.id})
-    # if not grains:
-    #     contexts.update({'error': u'%s 主机不存在或者离线' % tgt})
-    server_collects.delay(tgt=tgt,server_id=server_id,device=device0)
-    return HttpResponseRedirect("/cmdb/host_table/")
+
+    return render(request, 'cmdb/host_table.html', contexts)
+    # return HttpResponseRedirect("/cmdb/host_table/")
 
--- saltstack/tasks.py
+++ saltstack/tasks.py
@@ -10,7 +10,7 @@ def add(x, y):
     return x + y
 
 @task
-def server_collects(tgt,server_id,device):
+def server_collects(tgt,server_id):
     server_list = SaltServer.objects.all()
     contexts = {'server_list': server_list, 'server_id': server_id}
     try:
@@ -19,12 +19,13 @@ def server_collects(tgt,server_id,device):
         except Exception as e:  # id不存在时返回第一个
             salt_server = SaltServer.objects.all()[0]
     except Exception as e:
-        contexts.update({'error':e})
+        contexts.update({'error': e})
     try:
         sapi = SaltAPI(url=salt_server.url, username=salt_server.username, password=salt_server.password)
+        print salt_server.url
         grains = sapi.SaltCmd(tgt=tgt, fun='grains.items', client='local')['return'][0]
         minions = sapi.key_list('manage.status', client='runner')['return'][0]
-        if  salt_server and grains:
+        if salt_server and grains:
             for i in grains.keys():
                 try:
                     server = Servers.objects.get(local_ip=i)
@@ -35,7 +36,7 @@ def server_collects(tgt,server_id,device):
                 else:
                     minions_status = '1'
                 server.hostname = grains[i]['host']
-                server.local_ip = grains[i]['ip4_interfaces'][device][0]
+                server.local_ip = grains[i]['id']
                 server.OS = grains[i]['os'] + ' ' + grains[i]['osrelease'] + '-' + grains[i]['osarch']
                 server.Mem = grains[i]['mem_total']
                 server.Cpus = grains[i]['num_cpus']
@@ -43,12 +44,11 @@ def server_collects(tgt,server_id,device):
                 server.minion_id = grains[i]['id']
                 server.server_status = minions_status
                 server.save()
-                contexts.update({'success': u'%s 收集成功' % tgt,'server_id':salt_server.id})
+                contexts.update({'success': u'%s 收集成功' % tgt, 'server_id': salt_server.id})
         if not grains:
             contexts.update({'error': u'%s 主机不存在或者离线' % tgt})
-        return True
     except Exception as e:
-        contexts.update({'error':'%s %s' % (tgt,e)})
-        return False
+        contexts.update({'error': '%s %s' % (tgt, e)})
+    return contexts
 
 
