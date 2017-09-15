ccc569bd8990b6da9214de497edfeaceac284044
kbsonlong <13610018065@139.com>
2017-09-15 17:22:23 +0800

    zabbix修改

--- DockerWeb/models.py
+++ DockerWeb/models.py
@@ -5,7 +5,7 @@ class registry(models.Model):
     name = models.CharField(max_length=30,unique=True)
     address = models.CharField(max_length=100)
     version = models.IntegerField()
-    status = models.IntegerField(null=True)
+    status = models.IntegerField(null=True,default=0)
     isactive = models.IntegerField(null=True)
     auth = models.CharField(max_length=30,null=True)
 
